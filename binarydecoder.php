bc6c72c346292a4a8351f2dafe860b00fab08446
Joey de Ruiter <contact@joeyderuiter.me>
2018-04-10 21:18:32 +0200

    Added gzip binary decoder

---
+++ binarydecoder.php
@@ -0,0 +1,24 @@
+<?php
+
+$binlen = 8; # Bits that are required for hex decimal
+$binary = (string) file_get_contents('./binary.txt');
+$output = "";
+
+$filename = './challenge.txt.gz';
+
+if (file_exists($filename)) unlink($filename); 
+
+$fp = fopen($filename, 'w');
+
+for ($i = 0; $i < strlen($binary) / $binlen; $i++) {
+    $bin = substr($binary, $i * $binlen, $binlen);
+    $dec = bindec($bin);
+
+    $char = chr($dec);
+
+    echo $i . "\t" . $bin . "\t" . $char . PHP_EOL;
+
+    fwrite($fp, $char);
+}
+
+fclose($fp);
